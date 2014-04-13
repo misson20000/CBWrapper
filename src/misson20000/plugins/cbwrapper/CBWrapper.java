b2eaed31cd62f63e4fc3d1e4c6d394c833e98696
Misson20000/Xenotoad <xenotoad@xenotoad.net>
2014-04-13 13:00:35 -0700

    Fixed -o syntax, added -w flag, and made /cw work for console

--- src/misson20000/plugins/cbwrapper/CBWrapper.java
+++ src/misson20000/plugins/cbwrapper/CBWrapper.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
 
 public final class CBWrapper extends JavaPlugin {
     @Override
@@ -15,13 +16,36 @@ public final class CBWrapper extends JavaPlugin {
     @Override
     public void onDisable() {
     }
+
+    public int parseOffset(int orig, String offset) {
+	if(offset.startsWith("~")) {
+	    return orig + Integer.parseInt(offset.substring(1));
+	} else {
+	    return Integer.parseInt(offset);
+	}
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 	if (cmd.getName().equals("cb") || cmd.getName().equals("cw")) {
 		String name = sender.getName();
-		int ox = 0;
-		int oy = 0;
-		int oz = 0;
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		boolean haspos = false;
+		World world = null;
+		if((sender instanceof BlockCommandSender)) {
+		    Block block = ((BlockCommandSender)sender).getBlock();
+		    x = block.getLocation().getBlockX();
+		    y = block.getLocation().getBlockY();
+		    z = block.getLocation().getBlockZ();
+		    world = block.getLocation().getWorld();
+		    haspos = true;
+		}
+		if ((sender instanceof Player)) {
+		    return false;		    
+		}
+
 		int i;
 		for(i = 0; i < args.length; i++) {
 			if(!args[i].startsWith("-")) {
@@ -35,44 +59,52 @@ public final class CBWrapper extends JavaPlugin {
 					return false;
 				}
 			}
+			if(args[i].equals("-w")) {
+			    world = getServer().getWorld(args[++i]);
+			}
 			if(args[i].equals("-o")) {
-				if(args.length - ++i >= 3) {
-					ox = Integer.parseInt(args[i++].substring(1));
-					oy = Integer.parseInt(args[i++].substring(1));
-					oz = Integer.parseInt(args[i].substring(1));
-				} else {
-					sender.sendMessage("Not enough arguments for -o. Expected -o ~<x> ~<y> ~<z>");
-					return false;
+			    if(args.length - ++i >= 3) {
+				if(!haspos && args[i].startsWith("~")) {
+				    sender.sendMessage("The console has no position for a relative offset");
+				    return false;
 				}
+			        x = parseOffset(x, args[i++]);
+				if(!haspos && args[i].startsWith("~")) {
+				    sender.sendMessage("The console has no position for a relative offset");
+				    return false;
+				}
+				y = parseOffset(y, args[i++]);
+				if(!haspos && args[i].startsWith("~")) {
+				    sender.sendMessage("The console has no position for a relative offset");
+				}
+				z = parseOffset(z, args[i]);
+			    } else {
+				sender.sendMessage("Not enough arguments for -o. Expected -o <x> <y> <z>");
+				return false;
+			    }
 			}
 		}
-	    if (args.length > 0) {
-			String concatargs = "";
-			for (; i < args.length; i++) {
-			    concatargs = concatargs + args[i] + " ";
-			}
-			if ((sender instanceof Player)) {
-			    return false;
-			}
-			if ((sender instanceof ConsoleCommandSender)) {
-			    sender.sendMessage("Sorry, but this command is not intended for the console, because the console has no x, y, or z. Try running in a command block");
-			    return false;
-			}
-			if ((sender instanceof BlockCommandSender)) {
-			    Block block = ((BlockCommandSender)sender).getBlock();
-			    Player dummy = new DummyPlayer(name, getServer(),
-			        block.getLocation().getWorld(),
-					block.getLocation().getBlockX()+ox,
-					block.getLocation().getBlockY()+oy,
-	                block.getLocation().getBlockZ()+oz);
-			    getServer().dispatchCommand(dummy, concatargs);
-			}
-			return true;
-	    }
-	    sender.sendMessage("Error: The /cb command requires at least one parameter");
+		if (args.length > 0) {
+		    String concatargs = "";
+		    for (; i < args.length; i++) {
+			concatargs = concatargs + args[i] + " ";
+		    }
+		    if(world == null) {
+			sender.sendMessage("The -w flag is necessary for the console");
+			return false;
+		    }
+		    Player dummy = new DummyPlayer(name, getServer(),
+						   world,
+						   x, y, z);
+		    getServer().dispatchCommand(dummy, concatargs);
+		} else {
+		    sender.sendMessage("Not enough arguments");
+		    return false;
+		}
+		return true;
+	} else {
 	    return false;
 	}
-	
-	return false;
     }
 }
+
